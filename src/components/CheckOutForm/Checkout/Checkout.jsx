08f976fe226189434edc8ae3ef4b5c0b9214f2c6
madridtran <tranvanhungk39a1@gmail.com>
2022-03-22 17:58:34 +0700

    init Project

---
+++ src/components/CheckOutForm/Checkout/Checkout.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import {
+  Paper,
+  Stepper,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+  makeStyles,
+  Step,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import AddressForm from "../AddressForm";
+import PaymentForm from "../PaymentForm";
+import { commerce } from "../../lib/commerce";
+
+const steps = ["Shipping address", "Payment details"];
+
+const Checkout = ({ cart }) => {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
+  useEffect(() => {
+    const generateToken = async () => {
+      try {
+        const token = await commerce.checkout.generateToken(cart.id, {
+          type: "cart",
+        });
+        setCheckoutToken(token);
+      } catch (error) {}
+    };
+    generateToken();
+  }, [cart]);
+
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  const next = (data) => {
+    setShippingData(data);
+    nextStep()
+  };
+
+  const Confirmation = () => <div>Confirm</div>;
+  const Form = () =>
+    activeStep == 0 ? (
+      <AddressForm checkoutToken={checkoutToken} next={next} />
+    ) : (
+      <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken}/>
+    );
+
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            CheckOut
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep == steps.length ? (
+            <Confirmation />
+          ) : (
+            checkoutToken && <Form />
+          )}
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
